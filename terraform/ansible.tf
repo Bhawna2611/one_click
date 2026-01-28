3a13cfe45d90b51a30bde9e056ed807ecef7161f
Bhawna2611 <166822422+Bhawna2611@users.noreply.github.com>
2026-01-28 21:37:46 +0530

    Modify Ansible SSH args and set Python interpreter
    Updated SSH arguments for Ansible and added Python interpreter path.

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -4,7 +4,10 @@ resource "local_file" "ansible_inventory" {
 web_server ansible_host=${module.compute.private_ip} ansible_user=ubuntu
 
 [web:vars]
-ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q ubuntu@${module.compute.public_ip}"'
+ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -i /tmp/one__click.pem -W %h:%p -q -o StrictHostKeyChecking=no ubuntu@${module.compute.public_ip}"'
+ansible_python_interpreter=/usr/bin/python3
 EOT
   filename = "../ansible/inventory.ini"
+  directory_permission = "0777"
+  file_permission      = "0777"
 }
