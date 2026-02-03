d699ca64e9f7076d9f97a771f33129cec4e4a231
Bhawna Porwal <bhavna123porwal@gmail.com>
2026-02-03 11:30:18 +0530

    add ansible.tf

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -4,7 +4,7 @@ resource "local_file" "ansible_inventory" {
 web_server ansible_host=${module.compute.private_ip} ansible_user=ubuntu
 
 [web:vars]
-ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -i /tmp/one__click.pem -W %h:%p -q -o StrictHostKeyChecking=no ubuntu@${module.compute.public_ip}"'
+ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -i /tmp/one__click.pem -W %h:%p -q -o StrictHostKeyChecking=no ubuntu@${module.bastion.bastion_public_ip}"'
 ansible_python_interpreter=/usr/bin/python3
 EOT
   filename = "../ansible/inventory.ini"
