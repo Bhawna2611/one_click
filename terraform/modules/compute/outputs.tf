b261ad2147f87ec6c16f011dd3733425ce40b9ce
Bhawna Porwal <bhavna123porwal@gmail.com>
2026-02-03 11:21:53 +0530

    add ansible.tf

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -11,3 +11,11 @@ output "private_ips" {
   description = "List of private IPs for instances currently in the compute ASG"
   value       = data.aws_instances.asg_instances.private_ips
 }
+
+output "public_ip" {
+  value = aws_instance.your_instance_name.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.your_instance_name.private_ip
+}
